a2eede4f9fbde1ee70c5ad808b7244a3019e11c7
john-lallo <nuff97@gmail.com>
2015-07-30 18:55:06 +0800

    file organization.

---
+++ constraints/constraint_rev.js
@@ -0,0 +1,28 @@
+//constraint: rev constraint
+
+function rev_constraint(i, A, B, P){
+	constraint.call(this, i, [A, B]);
+
+	this.rA = numeric['-'](P, [this.S[0].q[0], this.S[0].q[1]]);
+	this.rA = rot(-this.S[0].q[2], this.rA);
+
+	this.rB = numeric['-'](P, [this.S[1].q[0], this.S[1].q[1]]);
+	this.rB = rot(-this.S[1].q[2], this.rB);
+}
+rev_constraint.prototype = Object.create(constraint.prototype);
+rev_constraint.prototype.constructor = rev_constraint;
+
+rev_constraint.prototype.C = function(){
+	var D = numeric['-'](
+		numeric['+'](
+			[this.S[0].q[0], this.S[0].q[1]],
+			rot(this.S[0].q[2], this.rA)
+		),
+		numeric['+'](
+			[this.S[1].q[0], this.S[1].q[1]],
+			rot(this.S[1].q[2], this.rB)
+		)
+	);
+
+	return numeric['dot'](D, D);
+}
